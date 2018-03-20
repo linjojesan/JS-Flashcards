d8310a2daea23720d2f932088ddf204a19cf2560
Bobby Martin and John Linneman <john@>
2018-03-20 15:55:25 -0700

    utilized jquery this keyword to change divs without using id tags

--- js/scripts.js
+++ js/scripts.js
@@ -1,59 +1,6 @@
 $().ready(function() {
-  $("#b1").click(function() {
-    $("#d1").toggleClass("active");
-  });
-  $("#b2").click(function() {
-    $("#d2").toggleClass("active");
-  });
-  $("#b3").click(function() {
-    $("#d3").toggleClass("active");
-  });
-  $("#b4").click(function() {
-    $("#d4").toggleClass("active");
-  });
-  $("#b5").click(function() {
-    $("#d5").toggleClass("active");
-  });
-  $("#b6").click(function() {
-    $("#d6").toggleClass("active");
-  });
-  $("#b7").click(function() {
-    $("#d7").toggleClass("active");
-  });
-  $("#b8").click(function() {
-    $("#d8").toggleClass("active");
-  });
-  $("#b9").click(function() {
-    $("#d9").toggleClass("active");
-  });
-  $("#b10").click(function() {
-    $("#d10").toggleClass("active");
-  });
-  $("#b11").click(function() {
-    $("#d11").toggleClass("active");
-  });
-  $("#b12").click(function() {
-    $("#d12").toggleClass("active");
-  });
-  $("#b13").click(function() {
-    $("#d13").toggleClass("active");
-  });
-  $("#b14").click(function() {
-    $("#d14").toggleClass("active");
-  });
-  $("#b15").click(function() {
-    $("#d15").toggleClass("active");
-  });
-  $("#b16").click(function() {
-    $("#d16").toggleClass("active");
-  });
-  $("#b17").click(function() {
-    $("#d17").toggleClass("active");
-  });
-  $("#b18").click(function() {
-    $("#d18").toggleClass("active");
-  });
-  $("#b19").click(function() {
-    $("#d19").toggleClass("active");
-  });
+  $(".definition").click(function() {
+    $(this).find(".define").toggle();
+
+    });
 });
